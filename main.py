0747ca50da1eb8d9c9d4d8df55ae293f1251b33d
RafaelaAfférri <rafaelaao@al.insper.edu.br>
2024-08-07 11:03:30 -0300

    exercico feito

---
+++ main.py
@@ -0,0 +1,32 @@
+def calculadora(conta):
+    res = 0
+    num1 = ''
+    num2 = ''
+    posicao_sinal = []
+    conta = conta.replace(" ", "")
+    for i in range(len(conta)):
+        if conta[i] == "+" or conta[i] == "-":
+            posicao_sinal.append(i)
+
+    for i in range(len(posicao_sinal)):
+        if res == 0:
+            num1 = conta[0:posicao_sinal[i]]
+            if len(posicao_sinal) == i+1:
+                num2 = conta[posicao_sinal[i]+1:]
+            else:
+                num2 = conta[posicao_sinal[i]+1:posicao_sinal[i+1]]
+        else:
+            num1 = res
+            if len(posicao_sinal) == i+1:
+                num2 = conta[posicao_sinal[i]+1:]
+            else:
+                num2 = conta[posicao_sinal[i]+1:posicao_sinal[i+1]]
+        if conta[posicao_sinal[i]] == "+":
+            res = int(num1) + int(num2)
+        if conta[posicao_sinal[i]] == "-":
+            res = int(num1) - int(num2)
+
+        
+    return res
+
+print(calculadora("7 8 9   +345    -     123"))
