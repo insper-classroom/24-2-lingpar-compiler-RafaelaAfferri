85090bcea466db1a060b0cd8d7d065c49563f806
RafaelaAfférri <rafaelaao@al.insper.edu.br>
2024-08-21 15:42:49 -0300

    limpa comentarios

--- main.py
+++ main.py
@@ -50,10 +50,116 @@
 
 # AULA 2
 
+# import sys
+
+# class Token():
+#     def __init__(self, tipo, valor):
+#         self.tipo = tipo
+#         self.valor = valor
+    
+# class Tokenizer():
+#     def __init__(self, source):
+#         self.source = source #codigo fonte
+#         self.position = 0 #posição atual
+#         self.next = None #ultimo token lido
+
+#     def selectNext(self):
+#         if self.position >= len(self.source):
+#             return Token('EOF', None)
+#         if self.source[self.position] == '+':
+#             self.position += 1
+#             self.next = Token('PLUS', '+')
+#             return self.next
+#         elif self.source[self.position] == '-':
+#             self.position += 1
+#             self.next = Token('MINUS', '-')
+#             return self.next
+#         elif self.source[self.position].isdigit():
+#             start = self.position
+#             while self.position < len(self.source) and self.source[self.position].isdigit():
+#                 self.position += 1
+#             self.next = Token('INT', int(self.source[start:self.position]))
+#             return self.next
+#         elif self.source[self.position].isspace():
+#             start = self.position
+#             while self.position < len(self.source) and self.source[self.position].isspace():
+#                 self.position += 1
+#             return self.selectNext()
+#         else:
+#             raise ValueError('Caracter inválido: ' + self.source[self.position])
+
+# class Parser():
+#     def __init__(self):
+#         self.tokenizer = None
+#         self.resultado = 0
+
+#     def parseExpression(self):
+        
+#         token = self.tokenizer.selectNext()
+#         if token.tipo == 'INT':
+#             self.resultado += token.valor
+#             token = self.tokenizer.selectNext()
+#             if token.tipo == 'INT':
+#                 raise ValueError('Token inválido: ' + token.tipo)
+#             while token.tipo == 'PLUS' or token.tipo == 'MINUS':
+#                 if token.tipo == 'PLUS':
+#                     token = self.tokenizer.selectNext()
+#                     if token.tipo == 'INT':
+#                         self.resultado += token.valor
+#                     else:
+#                         raise ValueError('Token inválido: ' + token.tipo)
+#                 elif token.tipo == 'MINUS':
+#                     token = self.tokenizer.selectNext()
+#                     if token.tipo == 'INT':
+#                         self.resultado -= token.valor
+#                     else:
+#                         raise ValueError('Token inválido: ' + token.tipo)
+#                 token = self.tokenizer.selectNext()
+#             return self.resultado
+#         else:
+#             raise ValueError('Token inválido: ' + token.tipo)
+
+            
+
+#     def run(self, code):
+#         tokenizador = Tokenizer(code)
+#         self.tokenizer = tokenizador
+#         return self.parseExpression()
+    
+
+# code = sys.argv[1]
+
+# parser = Parser()
+# resultado = parser.run(code)
+
+# print(resultado)
+
+#AULA 3 - mult e div
+
 import sys
 
-
-
+def limpa_coment(text):
+    achou = True
+    while achou:
+        achou = False
+        for i in range(len(text)-1):
+            if text[i] == "/" and text[i+1] == "*":
+                inicio = i
+                for j in range(i+2,(len(text)-1)):
+                    if text[j] == "*" and text[j+1] == "/":
+                        fim = j+2
+                        text = text[:inicio] + text[fim:] 
+                        achou = True
+                        break
+                if not achou:
+                    raise ValueError("Comentário não fechado")
+            if achou:
+                break
+            if text[i] == "*" and text[i+1] == "/":
+                raise ValueError("Comentário não aberto")
+        
+    
+    return text
 
 class Token():
     def __init__(self, tipo, valor):
@@ -62,7 +168,8 @@ class Token():
     
 class Tokenizer():
     def __init__(self, source):
-        self.source = source #codigo fonte
+        
+        self.source = limpa_coment(source) #codigo fonte
         self.position = 0 #posição atual
         self.next = None #ultimo token lido
 
@@ -76,6 +183,14 @@ class Tokenizer():
         elif self.source[self.position] == '-':
             self.position += 1
             self.next = Token('MINUS', '-')
+            return self.next
+        elif self.source[self.position] == '*':
+            self.position += 1
+            self.next = Token('MULT', '*')
+            return self.next
+        elif self.source[self.position] == '/':
+            self.position += 1
+            self.next = Token('DIV', '/')
             return self.next
         elif self.source[self.position].isdigit():
             start = self.position
@@ -96,33 +211,59 @@ class Parser():
         self.tokenizer = None
         self.resultado = 0
 
-    def parseExpression(self):
-        
+    def parseTerm(self):
         token = self.tokenizer.selectNext()
         if token.tipo == 'INT':
-            self.resultado += token.valor
+            self.resultado = token.valor
             token = self.tokenizer.selectNext()
             if token.tipo == 'INT':
                 raise ValueError('Token inválido: ' + token.tipo)
-            while token.tipo == 'PLUS' or token.tipo == 'MINUS':
-                if token.tipo == 'PLUS':
+            while token.tipo == 'MULT' or token.tipo == 'DIV':
+                if token.tipo == 'MULT':
                     token = self.tokenizer.selectNext()
                     if token.tipo == 'INT':
-                        self.resultado += token.valor
+                        self.resultado *= token.valor
                     else:
                         raise ValueError('Token inválido: ' + token.tipo)
-                elif token.tipo == 'MINUS':
+                elif token.tipo == 'DIV':
                     token = self.tokenizer.selectNext()
                     if token.tipo == 'INT':
-                        self.resultado -= token.valor
+                        self.resultado /= token.valor
                     else:
                         raise ValueError('Token inválido: ' + token.tipo)
                 token = self.tokenizer.selectNext()
             return self.resultado
         else:
             raise ValueError('Token inválido: ' + token.tipo)
+        
+    def parseExpression(self):
 
-            
+        
+        resultado = self.parseTerm()
+        token = self.tokenizer.next
+        if token.tipo != 'EOF':
+
+            while token.tipo == 'PLUS' or token.tipo == 'MINUS':
+                if token.tipo == 'PLUS':
+                    res = self.parseTerm()
+                    token = Token('INT', res)
+                    if token.tipo == 'INT':
+                        resultado += token.valor
+                    else:
+                        raise ValueError('Token inválido: ' + token.tipo)
+                elif token.tipo == 'MINUS':
+                    res = self.parseTerm()
+                    token = Token('INT', res)
+                    if token.tipo == 'INT':
+                        resultado -= token.valor
+                    else:
+                        raise ValueError('Token inválido: ' + token.tipo)
+                token = self.tokenizer.next
+            return resultado
+        else:
+            raise ValueError('Token inválido: ' + token.tipo)
+
+
 
     def run(self, code):
         tokenizador = Tokenizer(code)
@@ -136,4 +277,3 @@ parser = Parser()
 resultado = parser.run(code)
 
 print(resultado)
-
