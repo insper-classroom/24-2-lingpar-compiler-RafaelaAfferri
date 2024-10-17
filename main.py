dc3be24d93c67112af3be27ca42bbea2e8ed07db
RafaelaAfférri <rafaelaao@al.insper.edu.br>
2024-10-17 08:09:57 -0300

    .

--- main.py
+++ main.py
@@ -838,20 +838,6 @@ if __name__ == '__main__':
     filecode = sys.argv[1]
     with open(filecode, 'r') as file:
        code = file.read()
-#     code = """
-        
- 
-#   {
-#     int a;
-#     a = 5;
-#     int b;
-#     b = 10;
-#     int c;
-#     c = a+1;
-
-    
-# }
-#         """
 
     parser = Parser()
 
